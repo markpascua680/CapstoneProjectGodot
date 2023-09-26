14d441430629c43b84ac009010d279e498a216d9
Mark Pascua <markpascua680@yahoo.com>
2023-09-25 19:20:55 -0800

    Added GameStates enum class - Fixes #2

---
+++ Model/Game/GameStates.cs
@@ -0,0 +1,26 @@
+namespace Model.Game
+{
+    public static enum GameStates
+    {
+        TITLE_SCREEN = 0,
+
+        //Gameplay 
+        FREE_ROAM,
+
+        WILD_POKEMON_BATTLE,
+
+        TRAINER_POKEMON_BATTLE,
+
+        PAUSE,
+
+        // Menus
+        POKEMON_MENU,
+
+        INVENTORY_MENU,
+
+        // Fade to black when loading an area, then fade in to the scene
+        FADE_OUT,
+
+        FADE_IN
+    }
+}

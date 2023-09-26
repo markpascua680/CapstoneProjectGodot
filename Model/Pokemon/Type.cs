27d7b9f82a620a1eedf50d36e3e679857d63c576
Mark Pascua <markpascua680@yahoo.com>
2023-09-25 18:56:12 -0800

    Added Pokemon, Type, and Stats classes - Fixes #1

---
+++ Model/Pokemon/Type.cs
@@ -0,0 +1,39 @@
+namespace Model.Pokemon.Type
+{
+    public enum Type
+    {
+        NORMAL = 0,
+
+        FIRE,
+
+        WATER,
+
+        GRASS,
+
+        ELECTRIC,
+
+        ICE,
+
+        FIGHTING,
+
+        POISON,
+
+        GROUND,
+
+        FLYING,
+
+        PSYCHIC,
+
+        BUG,
+
+        ROCK,
+
+        GHOST,
+
+        DRAGON,
+
+        DARK,
+
+        STEEL
+    }
+}

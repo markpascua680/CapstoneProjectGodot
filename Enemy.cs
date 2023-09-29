c9accb07d84a87466aa67924d811f12779b42400
Ramirgarcia <84408466+Ramirgarcia@users.noreply.github.com>
2023-09-28 17:16:16 -0800

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -1,7 +1,10 @@
 using Godot;
 using System;
+// PseudoCode
+// using Player.Trainer
+// using NPCList
+// 
 
-var enemySprite = @GDScript.load("res://Assets/Pokemon-Tutorial-Art-Assets-master/Art/gfx/cuttable tree.png");
 
 
 public partial class Enemy : Sprite2D
@@ -9,7 +12,10 @@ public partial class Enemy : Sprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.Texture.set_texture(enemySprite);
+		// currEnemyEntity = NPCList 
+		var enemySprite = (Texture2D)GD.Load("res://Assets/Pokemon-Tutorial-Art-Assets-master/Art/gfx/cuttable tree.png");
+		
+		this.Texture = enemySprite;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

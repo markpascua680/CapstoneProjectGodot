e89629f4c9056719499839a22f2bf30833ba4866
Ramirgarcia <ramiro.i.garcia@outlook.com>
2023-10-09 13:30:38 -0800

    Added json and started on pulling information

---
+++ Controller/Scenes/node_2d.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class node_2d : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	var enemySprite = (Texture2D)GD.Load("res://Assets/Pokemon-Tutorial-Art-Assets-master/Art/gfx/cuttable tree.png");
+	GetNode<Sprite2D>("enemyEntity").Texture = enemySprite;
+	} 
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}

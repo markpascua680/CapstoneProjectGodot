6fdd6490a1d286c9aace710d323a18907e9f8c7a
Ramirgarcia <ramiro.i.garcia@outlook.com>
2023-10-14 22:14:33 -0800

    Parsed through JSONs

--- Controller/Scenes/node_2d.cs
+++ Controller/Scenes/node_2d.cs
@@ -1,17 +1,41 @@
 using Godot;
 using System;
 using Model.Characters;
+using System.IO;
+using Godot.Collections;
+using System.Linq;
+using System.Collections;
+using System.Text.Json.Serialization;
 public partial class node_2d : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	var enemySprite = (Texture2D)GD.Load("res://Assets/Pokemon-Tutorial-Art-Assets-master/Art/gfx/cuttable tree.png");
-	GetNode<Sprite2D>("enemyEntity").Texture = enemySprite;
+
+		var fileLocation = "res://Model/Characters/trainerConstant.json";
+		var file = Godot.FileAccess.Open(fileLocation, Godot.FileAccess.ModeFlags.Read);
+		var tmp = Json.ParseString(file.GetAsText());
+		GD.Print(tmp.AsGodotDictionary());
+		//var dict = Json.ParseString(tmp).AsGodotDictionary<string, Dictionary>();
+		//GD.Print(dict["trainerInfo"]["defaultTestEnemy"].GetType());
+
+		var statSheetDict = Json.ParseString(file.GetAsText());
+
+
+		
+		//GD.Print(statSheetDict.AsGodotDictionary<string, Variant.>);
+		//GD.Print(statSheetDict["trainerInfo"]);
+		
+		
+
+		//var dude1 = new Character{EntityTeam = };
+		var enemySprite = (Texture2D)GD.Load("res://Assets/Pokemon-Tutorial-Art-Assets-master/Art/gfx/cuttable tree.png");
+		GetNode<Sprite2D>("enemyEntity").Texture = enemySprite;
 	} 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
 	}
 }

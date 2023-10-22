57fd5b1b769cdcf2b6185063af6bb1fd889e938e
Ramirgarcia <ramiro.i.garcia@outlook.com>
2023-10-21 17:13:52 -0800

    just in case

--- Controller/Scenes/node_2d.cs
+++ Controller/Scenes/node_2d.cs
@@ -6,6 +6,7 @@ using Godot.Collections;
 using System.Linq;
 using System.Collections;
 using System.Text.Json.Serialization;
+using HelperFiles.trainerConstantHandler;
 public partial class node_2d : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
---
+++ HelperFiles/trainerConstantHandler.cs
@@ -0,0 +1,36 @@
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace HelperFiles.trainerConstantHandler;
+
+
+public class trainerConstant
+{
+private readonly string _sampleJsonFilePath = "Model\\Characters\\trainerConstant.json";
+public Dictionary<string,trainerIDName> getTrainerDict()
+{
+    using StreamReader reader = new(_sampleJsonFilePath);
+    var json = reader.ReadToEnd();
+    Dictionary<string,trainerIDName> trainerDict = JsonConvert.DeserializeObject<Dictionary<string,trainerIDName>>(json);
+    return trainerDict;
+
+ }
+}
+
+public class teamValues
+{
+    public int spriteID {get; set;}
+    public int lvl{get; set;}
+    public List<int> moveSetID{get; set;}
+    public List<int> equipMentID{get; set;}
+}
+
+public class trainerIDName
+{
+    public string name{get; set;}
+    public List<int> consumableID{get; set;}
+    public List<teamValues> entityTeam{get; set;}
+}

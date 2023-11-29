349ac51467dacb3cc7141e2a73ca2abcd383d35d
Ramirgarcia <ramiro.i.garcia@outlook.com>
2023-11-28 21:34:57 -0900

    Added Menus to Battle

--- Controller/Scenes/battleScene.cs
+++ Controller/Scenes/battleScene.cs
@@ -52,6 +52,6 @@ public partial class BattleScene : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		
 	}
 }
